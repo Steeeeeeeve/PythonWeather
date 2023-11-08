9ffd09a08b41a99f02d39074730d5e4673b5bcdb
Steeeeeeeve <steven.hurkett@gmail.com>
2023-11-08 08:41:12 -0600

    input for WeatherScraper

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,1 +1,10 @@
+from html.parser import HTMLParser
+from datetime import date
+import urllib.request
 
+day = date.day
+month = date.month
+year = date.year
+url = "http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=",year,"&Day=",day,"&Year=",year,"&Month=5"
+class WeatherScraper(url):
+    
